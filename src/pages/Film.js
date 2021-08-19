b1a8c2c3872ba5f411af035146e1202936cf6c49
kridt <chrnielsen2003@gmail.com>
2021-08-19 18:36:47 +0200

    link to movie

--- src/pages/Film.js
+++ src/pages/Film.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import "./Film.css";
+import { Link } from "@reach/router";
 
 export default function Film() {
     var [film, setFilm] = useState({});
@@ -15,7 +16,7 @@ export default function Film() {
        
         var inOrder = film.results;
 
-        inOrder.sort(function(a, b){
+        inOrder?.sort(function(a, b){
             return a.episode_id - b.episode_id;
         })
 
@@ -23,13 +24,13 @@ export default function Film() {
         
     return (
         <div>
-            <h1>Star Wars </h1>
+            <h1>Star Wars API</h1>
             <div>
                  <ol className="moviesOrder">
                     {film && film.results?.map((info)=>{
                         
                         return(
-                            <p key={info.episode_id}>{info.episode_id}. {info.title}</p>
+                            <Link to={"/movie/" + info.episode_id} key={info.episode_id}>{info.episode_id}. {info.title}</Link>
                         )
 
                     })}
